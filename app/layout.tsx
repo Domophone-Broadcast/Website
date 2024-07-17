0e4b396e396849fe8db769e5027ad4dca6ecbe13
JusDePoire <antonin@dutreix.net>
2024-07-17 23:29:53 +0200

    Adding all

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Domophone: The next generation doorbell",
+  description: "Domophone is a brand new way to alert you when someone is in front of your door.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
